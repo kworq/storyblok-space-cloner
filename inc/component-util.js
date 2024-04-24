89860f37323f536a4ee6c6e979a393347f3852c1
Guy Peires <guy@kworq.com>
2024-04-23 21:32:19 -0400

    component-utils - for reals this time

---
+++ inc/component-util.js
@@ -0,0 +1,75 @@
+import "dotenv/config";
+
+const { SOURCE_SPACE_ID, TARGET_SPACE_ID } = process.env;
+
+export async function copyComponents(sourceClient, targetClient) {
+  const s_response = await sourceClient.get(
+    `/spaces/${SOURCE_SPACE_ID}/components/`,
+    {}
+  );
+  const source_components = new Map();
+  const sourceComponents = s_response.data?.components ?? [s_response.data];
+  // console.log(sourceComponents);
+  // return;
+  sourceComponents?.forEach((component) => {
+    source_components.set(component.name, component);
+  });
+
+  const t_response = await targetClient.get(
+    `/spaces/${TARGET_SPACE_ID}/components/`,
+    {}
+  );
+  const target_components = new Map();
+  const targetComponents = t_response.data.components ?? [t_response.data];
+  targetComponents?.forEach((component) => {
+    target_components.set(component.name, component);
+  });
+  let count = 0;
+  for await (const [key, _component] of source_components) {
+    const { name, display_name, schema, image, is_root, is_nestable } =
+      _component;
+    const component = {
+      name,
+      display_name,
+      schema,
+      image,
+      is_root,
+      is_nestable,
+    };
+    const endpoint = `/spaces/${TARGET_SPACE_ID}/components/`;
+    const component_id = target_components.get(key)?.id;
+    if (component_id) {
+      try {
+        const res = await targetClient.put(`${endpoint}${component_id}`, {
+          component: { ...component, id: component_id },
+        });
+        const { id, name } = res.data.component;
+        console.log(
+          `Status: ${res.status} Updated component id: ${id} name: ${name}`
+        );
+        count++;
+      } catch (e) {
+        console.error(e);
+      }
+    } else {
+      try {
+        const res = await targetClient.post(endpoint, {
+          component,
+        });
+        const { id, name } = res.data.component;
+        console.log(
+          `Status: ${res.status} Created component id: ${id} name: ${name}`
+        );
+        count++;
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
+
+  return {
+    clone_type: "components",
+    count: count,
+    from_total: sourceComponents.length,
+  };
+}
