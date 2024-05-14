a92452260c2a357590074e73864002a07b09622e
Guy Peires <guy@kworq.com>
2024-05-13 21:43:32 -0400

    component groups copied and ref'd; copy components / component groups to disk

--- inc/component-util.ts
+++ inc/component-util.ts
@@ -1,14 +1,17 @@
 import "dotenv/config";
 import type StoryblokClient from "storyblok-js-client";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
 
 const { SOURCE_SPACE_ID, TARGET_SPACE_ID } = process.env;
-
-const pageLimit = 2;
-const per_page = 5;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export async function copyComponents(
   sourceClient: StoryblokClient,
   targetClient: StoryblokClient,
+  toDisk = true,
   created_count = 0,
   updated_count = 0
 ) {
@@ -17,11 +20,42 @@ export async function copyComponents(
     {}
   );
   const source_components = new Map();
+  const source_component_groups = new Map();
   const sourceComponents = s_response.data?.components ?? [s_response.data];
-  // console.log(sourceComponents);
-  // return;
+  const sourceGroups = s_response.data?.component_groups ?? [s_response.data];
+  const NOW = new Date().toISOString().replace(/:/g, "-");
+
+  if (toDisk) {
+    for await (const type of Object.keys(s_response.data)) {
+      const __newdirname = path.join(__dirname, "backups", NOW, type);
+      fs.mkdirSync(__newdirname, { recursive: true });
+      s_response.data[type].forEach(async (item: typeof s_response.data) => {
+        const jsonString = JSON.stringify(item, null, 2);
+
+        const filePath = path.join(__newdirname, `${item.name}.json`);
+
+        try {
+          await fs.promises.writeFile(filePath, jsonString);
+          console.log("Successfully wrote JSON to file:", filePath);
+        } catch (e) {
+          console.error("Error writing file:", e);
+        }
+      });
+    }
+
+    return {
+      clone_type: "components",
+      files_created: created_count,
+      components_copied: sourceComponents.length,
+      component_groups_copied: sourceGroups.length,
+    };
+  }
+
   sourceComponents?.forEach((component: typeof sourceComponents) => {
     source_components.set(component.name, component);
+  });
+  sourceGroups?.forEach((component: typeof sourceComponents) => {
+    source_component_groups.set(component.name, component);
   });
 
   const t_response = await targetClient.get(
@@ -29,13 +63,112 @@ export async function copyComponents(
     {}
   );
   const target_components = new Map();
+  const target_component_groups = new Map();
   const targetComponents = t_response.data.components ?? [t_response.data];
+  const targetGroups = t_response.data.component_groups ?? [t_response.data];
   targetComponents?.forEach((component: typeof targetComponents) => {
     target_components.set(component.name, component);
   });
+  targetGroups?.forEach((component: typeof targetGroups) => {
+    target_component_groups.set(component.name, component);
+  });
+
+  for await (const [key, _component_group] of source_component_groups) {
+    const { name } = _component_group;
+    const component_group = { name };
+    const endpoint = `/spaces/${TARGET_SPACE_ID}/component_groups/`;
+    const t_group = target_component_groups.get(key);
+    const component_group_id = t_group?.id;
+    if (component_group_id) {
+      try {
+        const res = await targetClient.put(`${endpoint}${component_group_id}`, {
+          component_group: {
+            ...component_group,
+            id: component_group_id,
+            parent_id: t_group.parent_id,
+          },
+        });
+        const { id, name } = res.data.component_group;
+        console.log(
+          `Status: ${res.status} Updated component group id: ${id} name: ${name}`
+        );
+        target_component_groups.set(name, res.data.component_group);
+        updated_count++;
+      } catch (e) {
+        console.error(e);
+      }
+    } else {
+      try {
+        const res = await targetClient.post(endpoint, {
+          component_group: {
+            ...component_group,
+          },
+        });
+        const { id, name } = res.data.component_group;
+        console.log(
+          `Status: ${res.status} Created component group id: ${id} name: ${name}`
+        );
+        target_component_groups.set(name, res.data.component_group);
+        created_count++;
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
+
+  // Update parent_id for component groups
+  for await (const [key, component_group] of source_component_groups) {
+    const endpoint = `/spaces/${TARGET_SPACE_ID}/component_groups/`;
+    const component_group_id = target_component_groups.get(key)?.id;
+    const component_group_parent_id =
+      source_component_groups.get(key)?.parent_id;
+
+    if (!component_group_parent_id) {
+      continue;
+    }
+
+    const sourceGroupArray = Array.from(source_component_groups);
+
+    const componentGroupParentName = sourceGroupArray.find(([name, group]) => {
+      return group.id === component_group_parent_id;
+    })?.[0];
+
+    const targetComponentGroupParentId = target_component_groups.get(
+      componentGroupParentName
+    )?.id;
+    try {
+      const res = await targetClient.put(`${endpoint}${component_group_id}`, {
+        component_group: {
+          ...component_group,
+          parent_id: targetComponentGroupParentId,
+        },
+      });
+      target_component_groups.set(key, res.data.component_group);
+      const { id, name } = res.data.component_group;
+
+      console.log(
+        `Status: ${res.status} Updated parent_id ${targetComponentGroupParentId} for component group id: ${id} name: ${name}`
+      );
+      //updated_count++;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  // Create or update components
   for await (const [key, _component] of source_components) {
     const { name, display_name, schema, image, is_root, is_nestable } =
       _component;
+
+    const sourceComponentGroupParentName = Array.from(
+      source_component_groups
+    ).find(([name, group]) => {
+      return group.uuid === _component.component_group_uuid;
+    })?.[0];
+    const component_group_uuid = target_component_groups.get(
+      sourceComponentGroupParentName
+    )?.uuid;
+
     const component = {
       name,
       display_name,
@@ -43,6 +176,7 @@ export async function copyComponents(
       image,
       is_root,
       is_nestable,
+      component_group_uuid,
     };
     const endpoint = `/spaces/${TARGET_SPACE_ID}/components/`;
     const component_id = target_components.get(key)?.id;
@@ -52,6 +186,7 @@ export async function copyComponents(
           component: { ...component, id: component_id },
         });
         const { id, name } = res.data.component;
+        target_components.set(name, res.data.component);
         console.log(
           `Status: ${res.status} Updated component id: ${id} name: ${name}`
         );
@@ -65,6 +200,7 @@ export async function copyComponents(
           component,
         });
         const { id, name } = res.data.component;
+        target_components.set(name, res.data.component);
         console.log(
           `Status: ${res.status} Created component id: ${id} name: ${name}`
         );
@@ -76,9 +212,9 @@ export async function copyComponents(
   }
 
   return {
-    clone_type: "components",
+    clone_type: "components / component_groups",
     created: created_count,
     updated: updated_count,
-    from_total: sourceComponents.length,
+    from_total: sourceComponents.length + sourceGroups.length,
   };
 }
