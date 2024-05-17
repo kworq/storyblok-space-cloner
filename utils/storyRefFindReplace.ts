6f740298ca4b9bf69235a9736e303ad928cb5773
Guy Peires <guy@kworq.com>
2024-05-17 13:42:26 -0400

    clean up

--- utils/storyRefFindReplace.ts
+++ utils/storyRefFindReplace.ts
@@ -1,31 +1,3 @@
-// Helper function to find UUIDs in a nested object
-// export function findUUIDs(obj: Record<string, any> | string | any[] | null) {
-//   const uuidRegex =
-//     /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-//   const uuids: string[] = [];
-
-//   function search(obj: Record<string, any> | string | any[] | null) {
-//     if (Array.isArray(obj)) {
-//       obj.forEach((item) => search(item));
-//     } else if (obj !== null && typeof obj === "object") {
-//       Object.entries(obj).forEach(([key, value]) => {
-//         if (
-//           typeof value === "string" &&
-//           uuidRegex.test(value) &&
-//           key !== "_uid"
-//         ) {
-//           uuids.push(value);
-//         } else if (typeof value === "object") {
-//           search(value);
-//         }
-//       });
-//     }
-//   }
-
-//   search(obj);
-//   return uuids;
-// }
-
 export function findUUIDs(obj: Record<string, any> | string | any[] | null) {
   const uuidRegex =
     /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
@@ -69,25 +41,6 @@ export function findUUIDs(obj: Record<string, any> | string | any[] | null) {
   return uuids;
 }
 
-// // Simulated function to fetch data by UUID
-// async function fetchDataByUUID(uuid: string) {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve({ full_slug: `path/to/resource/${uuid}` });
-//     }, 1000);
-//   });
-// }
-
-// // Simulated function to fetch another UUID by full_slug
-// async function fetchAnotherUUIDByFullSlug(fullSlug) {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       const newUUID = "new-" + fullSlug.split("/").pop();
-//       resolve(newUUID);
-//     }, 1000);
-//   });
-// }
-
 // Function to get a map of old UUIDs to new UUIDs
 export async function getFullSlugUUIDs(
   uuids: string[],
@@ -110,27 +63,6 @@ export async function getFullSlugUUIDs(
   }
   return { uuidMapping, mappingChanged };
 }
-
-// Function to replace UUIDs in the nested object
-// export function replaceUUIDs(
-//   obj: Record<string, any> | string | any[] | null,
-//   uuidMapping: Record<string, string>
-// ) {
-//   function replace(obj: Record<string, any> | string | any[] | null) {
-//     if (Array.isArray(obj)) {
-//       obj.forEach((item) => replace(item));
-//     } else if (obj !== null && typeof obj === "object") {
-//       Object.keys(obj).forEach((key) => {
-//         if (typeof obj[key] === "string" && uuidMapping[obj[key]]) {
-//           obj[key] = uuidMapping[obj[key]];
-//         } else if (typeof obj[key] === "object") {
-//           replace(obj[key]);
-//         }
-//       });
-//     }
-//   }
-//   replace(obj);
-// }
 
 export function replaceUUIDs(
   obj: Record<string, any> | string | any[] | null,
@@ -157,15 +89,3 @@ export function replaceUUIDs(
   }
   replace(obj);
 }
-
-// Main function to update UUIDs in a nested object
-// async function updateNestedObjectUUIDs(obj) {
-//   const uuids = findUUIDs(obj);
-//   console.log("Original UUIDs found:", uuids);
-
-//   const uuidMapping = await getFullSlugUUIDs(uuids);
-//   console.log("UUID mapping:", uuidMapping);
-
-//   replaceUUIDs(obj, uuidMapping);
-//   console.log("Updated object:", JSON.stringify(obj));
-// }
